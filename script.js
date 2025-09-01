780dae16901129b66f3e8908bc31bc86737455b0
parthapratim09 <parthapratim09@gmail.com>
2025-09-01 11:57:27 +0530

    update UI

--- script.js
+++ script.js
@@ -111,8 +111,4 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             fetchWeather('Kolkata');
-<<<<<<< HEAD
         });
-=======
-        });
->>>>>>> e9330b004a6c6fe11d073a77a6e08f3089d578c1
